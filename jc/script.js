741687ff4de4621da807125f5c9ba5dc97c76cae
Patrick Morais <ppternunes@gmail.com>
2021-04-29 14:01:28 -0300

    jc inicio

---
+++ jc/script.js
@@ -0,0 +1,41 @@
+const valor = document.querySelector('#valor');
+const parcelas = document.querySelector('#parcelas');
+const button = document.querySelector('button');
+const totalSpan = document.querySelector('#total');
+const jurosSpan = document.querySelector('#juros');
+
+const checkValor = () => {
+  if(!valor.value){
+    alert('Digite um valor válido!')
+  } else {
+  checkParcelas();
+  }
+}
+
+const showInfos = (totalAPagar, juros) => {
+  totalSpan.innerHTML = totalAPagar.toFixed(2);
+  jurosSpan.innerHTML = juros.toFixed(2);
+}
+
+//FV = PV x (1+i)n
+
+const calcularJuros = (param) => {
+  const valorInicial = parseFloat(valor.value);
+  const numeroDeParcelas = param - 1;
+  const jurosPorcentagem = 5/100;
+  const totalAPagar = valorInicial*[(1+jurosPorcentagem)**numeroDeParcelas]
+  const juros = totalAPagar - valorInicial
+  showInfos(totalAPagar, juros)
+}
+
+const checkParcelas = () => {
+  if(parcelas.value === 'Selecione as parcelas'){
+    alert('Selecione parcelas válidas!')
+  } else {
+  calcularJuros(parseInt(parcelas.value));
+  }
+}
+
+button.addEventListener('click', () => {
+  checkValor();
+})

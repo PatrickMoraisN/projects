55b6ea8d01f78d7046e7cb8c43960ff37980cb22
Patrick Morais <ppternunes@gmail.com>
2021-06-13 08:29:42 -0300

    side menu

--- side menu/script.js
+++ side menu/script.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.toggle-btn');
 const header = document.querySelector('.main-header');
 const over = document.querySelector('.overlay');
+const links = document.querySelectorAll('.links');
 
 const addOpen = () => {
   btn.classList.toggle('open')
@@ -9,3 +10,4 @@ const addOpen = () => {
 }
 
 btn.addEventListener('click', addOpen)
+links.forEach((link) => link.addEventListener('click', addOpen))

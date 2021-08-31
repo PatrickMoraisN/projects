b68168171a3db08455d2103f1dce72655a2a64d3
Patrick Morais <ppternunes@gmail.com>
2021-08-31 16:36:21 -0300

    feat: :sparkles: add query string search

--- recipes-api/index.js
+++ recipes-api/index.js
@@ -15,6 +15,12 @@ app.get('/recipes', async function (req, res) {
   res.json(recipes);
 });
 
+app.get('/recipes/search', function (req, res) {
+  const { name } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name));
+  res.status(200).json(filteredRecipes);
+});
+
 app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
   const recipe = recipes.find((r) => r.id === parseInt(id));

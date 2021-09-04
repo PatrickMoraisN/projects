11e2367d498906714e0a3ca15a18063d76ed5e35
Patrick Morais <ppternunes@gmail.com>
2021-09-04 11:47:03 -0300

    fix author id if null

--- model_mysql/index.js
+++ model_mysql/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const Author = require('./models/Author');
 const app = express()
 const port = 3000
+const STATUS_OK = 200;
+const STATUS_ERROR_NOTFOUND = 404;
 const messageDefault = {
   message: 'Hello, welcome to the default page!',
+}
+const messageNotFound = {
+  message: 'Page Not Found',
 }
 
 app.get('/', async (_req, res) => {
@@ -18,6 +23,8 @@ app.get('/authors', async (_req, res) => {
 app.get('/authors/:id', async (req, res) => {
   const { id } = req.params;
   const authors = await Author.findAuthorById(id);
+
+  if (!authors) return res.status(404).json(messageNotFound)
   res.status(200).json(authors)
 });
 

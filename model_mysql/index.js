5f001ad0d195c81d6d68283e50862646d0576c5c
Patrick Morais <ppternunes@gmail.com>
2021-09-04 13:08:54 -0300

    refactor error create / ok created

--- model_mysql/index.js
+++ model_mysql/index.js
@@ -4,6 +4,7 @@ const Author = require('./models/Author');
 const app = express();
 const port = 3000;
 const STATUS_OK_CREATED = 201;
+const STATUS_ERROR_CREATE = 400;
 const STATUS_OK = 200;
 const STATUS_ERROR_NOTFOUND = 404;
 const messageDefault = {
@@ -30,11 +31,11 @@ app.get('/authors', async (_req, res) => {
 app.post('/authors', async (req, res) => {
   const {first_name, middle_name, last_name} = req.body;
 
-  if(!Author.isValid(first_name, middle_name, last_name)) return res.status(400).json(messageInvalidData)
+  if(!Author.isValid(first_name, middle_name, last_name)) return res.status(STATUS_ERROR_CREATE).json(messageInvalidData)
 
   await Author.createAuthor(first_name, middle_name, last_name);
 
-  return res.status(201).json({message: 'Author Created!'})
+  return res.status(STATUS_OK_CREATED).json({message: 'Author Created!'})
 });
 
 app.get('/authors/:id', async (req, res) => {

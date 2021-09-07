7579133cbbee1dc162521a33c2695ad53b71a9c9
Patrick Morais <ppternunes@gmail.com>
2021-09-06 21:50:19 -0300

    add methods totalValue, addProducts, removeProducts

--- stock-intOO-java/entities/Product.java
+++ stock-intOO-java/entities/Product.java
@@ -1,3 +1,19 @@
-public class product {
-  
+package entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
 }

0f0a2f7481c213c423e855601edebfefa6a74ed7
Patrick Morais <ppternunes@gmail.com>
2021-09-09 00:04:12 -0300

    input focus

--- hidden-search-widget/script.js
+++ hidden-search-widget/script.js
@@ -1,6 +1,8 @@
 const buttonSearch = document.querySelector('.search button');
 const searchContainer = document.querySelector('.search');
+const inputSearch = document.querySelector('.search input');
 
 buttonSearch.addEventListener('click', () => {
   searchContainer.classList.toggle('active');
+  inputSearch.focus();
 })

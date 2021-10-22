837465f23dea19efa0e07cf5a9072ef566f6282e
Patrick Morais <ppternunes@gmail.com>
2021-10-21 23:34:32 -0300

    socket chat parte 1

---
+++ socket-chat/index.js
@@ -0,0 +1,32 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000/',
+    method: ['GET, POST' ]
+  }
+})
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('Alguem se conectou')
+
+  socket.on('disconnect', () => {
+    console.log('vish alguem saiu INFERNO')
+  })
+
+  socket.on('message', (msg) => {
+    console.log(msg)
+  })
+})
+
+app.get('/', (_req, res) => {
+  return res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000')
+})

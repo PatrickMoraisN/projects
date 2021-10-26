28c256f085464413cb86fc4a776ff72077a548ca
Patrick Morais <ppternunes@gmail.com>
2021-10-25 21:47:11 -0300

    db connect

--- notes-mongo/index.js
+++ notes-mongo/index.js
@@ -6,10 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
+// DB
+const db = require('./db/connection');
+
 // Template Engine
 app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars')
-app.use(express.static('public'))
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes import
 const notesRoutes = require('./routes/notes');
@@ -20,6 +24,13 @@ app.get('/', (_req, res) => {
 
 app.use('/notes', notesRoutes)
 
-app.listen(port, () => {
-  console.log(`Online at port ${port}`)
+db.initDb((err, db) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Banco conectado")
+    app.listen(port, () => {
+      console.log(`Online at port ${port}`)
+    })
+  }
 })

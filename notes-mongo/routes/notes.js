760b6cc9f5c75f3d6cff92164714e409f23b4699
Patrick Morais <ppternunes@gmail.com>
2021-10-25 21:58:32 -0300

    fix name handlebars / create post notes route

--- notes-mongo/routes/notes.js
+++ notes-mongo/routes/notes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const db = require('../db/connection');
+const { ObjectId } = require('mongodb');
 
 router.get('/', function(_req, res) {
   res.render('notes/create')
+})
+
+router.post('/', function(req, res) {
+  const { title, description } = req.body;
+
+  db.getDb()
+    .db()
+    .collection('notes')
+    .insertOne({ title, description });
+
+  return res.redirect(301, '/')
 })
 
 module.exports = router;

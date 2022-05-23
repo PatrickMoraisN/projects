07415d1908d3100f3d98cc021969ac6fe26d6bab
Patrick Morais <ppternunes@gmail.com>
2022-05-23 12:23:34 -0300

    feat: configure session storage

--- thoughts_mvc/index.js
+++ thoughts_mvc/index.js
@@ -8,6 +8,35 @@ require('dotenv').config();
 const app = express();
 
 const connection = require('./database/connection');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({
+  extended: true,
+}))
+app.use(express.json());
+
+app.use(
+  session({
+    name: "session",
+    secret: "our_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function() {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000, // One day
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    }
+  }),
+)
+
+
 const PORT = process.env.PORT || 3000
 
 connection
